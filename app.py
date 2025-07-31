39c0edc79787f69da0216ae4815b081aa577c6c1
Andrei Busu <andreiioanbusu@icloud.com>
2025-07-31 16:00:15 +1000

    almost done

--- app.py
+++ app.py
@@ -1,35 +1,92 @@
 from flask import render_template, request, redirect, url_for 
 from models import db, Project, app
+from flask_sqlalchemy import SQLAlchemy
+import datetime
 
+
+def check_db_exists():
+    if len(Project.query.all()) > 3:
+        return
+    
+    else :
+        initialized_project = []
+
+        if not Project.query.filter(Project.title == 'Guessing Game').first():
+            initialized_project.append(Project(title='Guessing Game', date_created=datetime.datetime(2025, 1, 7), description='This is a game where you have to guess the number before your lives run out', 
+                                    skills_learnt='For loops, If statements, Random module, Basic Python',
+                                    github_link="https://github.com/AndreiBusu123/Guessing-Game.git"))
+            
+        if not Project.query.filter(Project.title == 'Basketball Stats Tool').first():
+            initialized_project.append(Project(title='Basketball Stats Tool',date_created=datetime.datetime(2025, 3, 7), description='This is a tool that allows you to view the stats of your favorite basketball players', 
+                                    skills_learnt='Data cleanup, even more data cleanup, and more data cleanup',
+                                    github_link="https://github.com/AndreiBusu123/basketball-stats-tool-Andrei.git"))
+            
+        if not Project.query.filter(Project.title == 'Phrase Hunters').first():
+            initialized_project.append(Project(title='Phrase Hunters',date_created=datetime.datetime(2025, 5, 7), description='This is a game where you have to guess the number before your lives run out', 
+                                    skills_learnt='Multiple files, Classes, Methods, and more Python',
+                                    github_link="https://github.com/AndreiBusu123/Phrase-Hunters.git"))
+            
+        if not Project.query.filter(Project.title == 'Store Inventory with SQLAlchemy').first():
+            initialized_project.append(Project(title='Store Inventory with SQLAlchemy',date_created=datetime.datetime(2025, 7, 21), description='This is a game where you have to guess the number before your lives run out', 
+                                    skills_learnt='SQLAlchemy, Databases, sessions, and a little parsaltongue (Python)',
+                                    github_link="https://github.com/AndreiBusu123/Store-Inventory-With-SQLAlchemy.git"))
+            
+        
+        db.session.add_all(initialized_project)
+        db.session.commit()
+        return
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    projects = Project.query.all()
+    return render_template('index.html', projects=projects)
 
 
-@app.route('/projects/new')
+@app.route('/project/new')
 def new_project():
-    return render_template('create.html')
+    projects = Project.query.all()
+    return render_template('create.html', projects=projects)
 
 
-@app.route('/projects/<id>')
+@app.route('/project/<id>')
 def detail(id):
-    return render_template('detail.html')
+    project = Project.query.get_or_404(id)
+    projects = Project.query.all()
+    return render_template('detail.html', project=project, projects=projects)
 
 
-@app.route('/edit.html')
+@app.route('/project/<id>/edit', methods=['GET', 'POST'])
 def edit(id):
-    return render_template('edit.html')
+    project = Project.query.get_or_404(id)
+    projects = Project.query.all()
+    
+    if request.method == 'POST':
+        # Update the project with form data
+        project.title = request.form['title']
+        project.description = request.form['description']
+        project.skills_learnt = request.form['skills_learnt']
+        project.github_link = request.form['github_link']
+        
+        # Handle date conversion
+        if request.form['date_created']:
+            project.date_created = datetime.datetime.strptime(request.form['date_created'], '%Y-%m-%d')
+        
+        db.session.commit()
+        return redirect(url_for('detail', id=project.id))
+    
+    return render_template('edit.html', project=project, projects=projects)
 
-
-@app.route('/delete/<id>')
+@app.route('/project/<id>/delete')
 def delete(id):
-    return render_template('delete.html')
-
+    project = Project.query.get_or_404(id)
+    projects = Project.query.all()
+    return render_template('delete.html', project=project, projects=projects)
 
 
 if __name__ == '__main__':
+
     with app.app_context():
         db.create_all()
+        check_db_exists()
     app.run(debug=True, port=8000, host='0.0.0.0')
 
--- models.py
+++ models.py
@@ -12,13 +12,14 @@ db = SQLAlchemy(app)
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column('Title',db.String(100), nullable=False)
+    date_created = db.Column('Date Created', db.DateTime)
     description = db.Column('Description', db.String(500), nullable=False)
-    date_created = db.Column('Date Created', db.DateTime, default=datetime.datetime.now)
-    date_updated = db.Column('Date Updated', db.DateTime, default=datetime.datetime.now)
+    skills_learnt = db.Column('Skills Learnt', db.String(200), nullable=False)
+    github_link = db.Column('GitHub Link', db.String(200), nullable=False)
+
 
     def __repr__(self):
         return f'''<Project( Title: {self.title},
         Description: {self.description}, 
-        Date Created: {self.date_created}, 
-        Date Updated: {self.date_updated} )>'''
-    
+        Skills Learnt: {self.skills_learnt},
+        GitHub Link: {self.github_link} )>'''
